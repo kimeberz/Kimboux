0b2641955a58d097fa63554b674724864511f4a3
kimeberz <kimberly.eberz@rightscale.com>
2014-06-15 15:49:00 -0700

    JS magic added by Bo!

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
+(function (window) {
+	'use strict';
+	// Make nav work.
+	if (!"onhashchange" in window) {
+	    return;
+	}
+
+	var defaultSection = '#resume';
+
+	$('.navlink').click(function(e) {
+		e.preventDefault();
+		window.location.hash = $(this).attr('href');
+	});
+
+	var $sections = $('.js-section');
+
+	function locationHashChanged() {
+		$sections.hide();
+		var section = location.hash || defaultSection;
+		$(section).show();
+
+		$('.navlink').removeClass('nav-active');
+		$('.navlink[href="'+ section+ '"]').addClass('nav-active');
+	}
+
+	window.onhashchange = locationHashChanged;
+
+	locationHashChanged();
+}(window));
+
 
